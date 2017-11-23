65f47eacfdd477333b218f91c2090673b65f29c0
Rubtsov Aleksey <arubtsov@parma-telecom.ru>
2017-11-23 11:14:25 +0500

    Добавьте файлы проекта.

---
+++ EFDataApp2/Models/Phone.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFDataApp2.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } // название смартфона
+        public string Company { get; set; } // компания
+        public int Price { get; set; } // цена
+
+
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Artist { get; set; }
+        public string Country { get; set; }
+        public string Style { get; set; }
+        public string Album { get; set; }
+        public decimal Year { get; set; }
+        public string Label { get; set; }
+        public string Number { get; set; }
+        public string Licensing { get; set; }
+        public decimal Price { get; set; }
+        public string Cover { get; set; }
+        public string Booklet { get; set; }
+        public string Picture { get; set; }
+        public string Tracks { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+
+
+
+    }
+    public class Order
+    {
+        // [BindNever]
+        public int OrderId { get; set; }
+        // [BindNever]
+
+        //   public ICollection<CartLine> Lines { get; set; }
+
+        public string Family { get; set; }
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public string Index { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public bool GiftWrap { get; set; }
+        public bool Dispatched { get; set; }
+        public string EMail { get; set; }
+        //    public virtual List<OrderLine> OrderLines { get; set; }
+        public virtual List<OrderLine> OrderLines { get; set; }
+
+    }
+
+    public class OrderLine
+    {
+        public int OrderLineId { get; set; }
+        public Order Order { get; set; }
+       public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+
+}
---
+++ EFDataApp2/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using EFDataApp2.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace EFDataApp2
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // получаем строку подключения из файла конфигурации
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            // добавляем контекст MobileContext в качестве сервиса в приложение
+          //  services.AddDbContext<MobileContext>(options =>
+          //      options.UseSqlServer(connection));
+
+           // string connection2 = Configuration.GetConnectionString("ConnectionString");
+            services.AddDbContext < AppIdentityDbContext > (options =>
+                options.UseSqlServer(connection));
+
+            // services.AddDbContext<AppIdentityDbContext>(options =>
+            //options.UseSqlServer(Configuration["Data:MDSIdentity:ConnectionString"]));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<AppIdentityDbContext>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseIdentity();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
