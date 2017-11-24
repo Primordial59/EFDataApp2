b24596d9cdf246ae19509c550d317f4e3b4ea122
Алексей Рубцов <primordial59@gmail.com>
2017-11-24 13:51:02 +0500

    Добавлена процедура Создания Пользователя.

--- EFDataApp2/Controllers/AdminController.cs
+++ EFDataApp2/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using EFDataApp2.Models;
+using System.Threading.Tasks;
 
 namespace EFDataApp2.Controllers
 {
@@ -12,6 +13,38 @@ namespace EFDataApp2.Controllers
             userManager = usrMgr;
         }
         public ViewResult Index() => View(userManager.Users);
-        
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                MDSUser user = new MDSUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber
+                };
+                IdentityResult result
+                = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+
     }
 }
