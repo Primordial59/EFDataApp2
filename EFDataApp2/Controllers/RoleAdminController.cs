6c9309ed811faf83e651bac16e5562fc68ad1b5a
Алексей Рубцов <primordial59@gmail.com>
2017-12-01 18:15:16 +0500

    Построен административный функционал добавления, редактирвоания и удаления членов Ролей.

--- EFDataApp2/Controllers/RoleAdminController.cs
+++ EFDataApp2/Controllers/RoleAdminController.cs
@@ -17,12 +17,12 @@ namespace EFDataApp2.Controllers
         
      //   [Authorize(Roles = "Admins")]
         private RoleManager<IdentityRole> roleManager;
-      //  private UserManager<MDSUser> userManager;
+        private UserManager<MDSUser> userManager;
 
-            public RoleAdminController(RoleManager<IdentityRole> roleMgr)
+            public RoleAdminController(RoleManager<IdentityRole> roleMgr, UserManager<MDSUser> userMrg)
             {
                 roleManager = roleMgr;
-       //         userManager = userMrg;
+                userManager = userMrg;
             }
 
             public ViewResult Index() => View(roleManager.Roles);
@@ -71,71 +71,71 @@ namespace EFDataApp2.Controllers
                 return View("Index", roleManager.Roles);
             }
 
-            //public async Task<IActionResult> Edit(string id)
-            //{
+        public async Task<IActionResult> Edit(string id)
+        {
 
-            //    IdentityRole role = await roleManager.FindByIdAsync(id);
-            //    List<MDSUser> members = new List<MDSUser>();
-            //    List<MDSUser> nonMembers = new List<MDSUser>();
-            //    foreach (MDSUser user in userManager.Users)
-            //    {
-            //        var list = await userManager.IsInRoleAsync(user, role.Name)
-            //        ? members : nonMembers;
-            //        list.Add(user);
-            //    }
-            //    return View(new RoleEditModel
-            //    {
-            //        Role = role,
-            //        Members = members,
-            //        NonMembers = nonMembers
-            //    });
-            //}
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            List<MDSUser> members = new List<MDSUser>();
+            List<MDSUser> nonMembers = new List<MDSUser>();
+            foreach (MDSUser user in userManager.Users)
+            {
+                var list = await userManager.IsInRoleAsync(user, role.Name)
+                ? members : nonMembers;
+                list.Add(user);
+            }
+            return View(new RoleEditModel
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+        }
 
-            //[HttpPost]
-            //public async Task<IActionResult> Edit(RoleModificationModel model)
-            //{
-            //    IdentityResult result;
-            //    if (ModelState.IsValid)
-            //    {
-            //        foreach (string userId in model.IdsToAdd ?? new string[] { })
-            //        {
-            //            MDSUser user = await userManager.FindByIdAsync(userId);
-            //            if (user != null)
-            //            {
-            //                result = await userManager.AddToRoleAsync(user,
-            //                model.RoleName);
-            //                if (!result.Succeeded)
-            //                {
-            //                    AddErrorsFromResult(result);
-            //                }
-            //            }
-            //        }
-            //        foreach (string userId in model.IdsToDelete ?? new string[] { })
-            //        {
-            //            MDSUser user = await userManager.FindByIdAsync(userId);
-            //            if (user != null)
-            //            {
-            //                result = await userManager.RemoveFromRoleAsync(user,
-            //                model.RoleName);
-            //                if (!result.Succeeded)
-            //                {
-            //                    AddErrorsFromResult(result);
-            //                }
-            //            }
-            //        }
-            //    }
+        [HttpPost]
+        public async Task<IActionResult> Edit(RoleModificationModel model)
+        {
+            IdentityResult result;
+            if (ModelState.IsValid)
+            {
+                foreach (string userId in model.IdsToAdd ?? new string[] { })
+                {
+                    MDSUser user = await userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await userManager.AddToRoleAsync(user,
+                        model.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            AddErrorsFromResult(result);
+                        }
+                    }
+                }
+                foreach (string userId in model.IdsToDelete ?? new string[] { })
+                {
+                    MDSUser user = await userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await userManager.RemoveFromRoleAsync(user,
+                        model.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            AddErrorsFromResult(result);
+                        }
+                    }
+                }
+            }
 
-            //    if (ModelState.IsValid)
-            //    {
-            //        return RedirectToAction(nameof(Index));
-            //    }
-            //    else
-            //    {
-            //        return await Edit(model.RoleId);
-            //    }
-            //}
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return await Edit(model.RoleId);
+            }
+        }
 
-            private void AddErrorsFromResult(IdentityResult result)
+        private void AddErrorsFromResult(IdentityResult result)
             {
                 foreach (IdentityError error in result.Errors)
                 {
--- EFDataApp2/Models/UserViewModels.cs
+++ EFDataApp2/Models/UserViewModels.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
-
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace EFDataApp2.Models
 {
@@ -23,7 +24,37 @@ namespace EFDataApp2.Models
         [UIHint ("password")]
         public string Password { get; set; }
       
+    }
+
+    public class LoginModel
+    {
+        [Required]
+        [UIHint("email")]
+        public string Email { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+
 
     }
+    public class RoleEditModel
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<MDSUser> Members { get; set; }
+        public IEnumerable<MDSUser> NonMembers { get; set; }
+
+    }
+    public class RoleModificationModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public string RoleId { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
+
+
+    }
+
 
 }
